9df271002a928576326398fca9ea15574de60950
TanThanh-CityBoi <tanthanhe@gmail.com>
2022-07-15 12:28:13 +0700

    set up app

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5000; // port number
+const app = express()
+const route = require("./routers/index"); // router impl
+var morgan = require("morgan");
+
+app.use(morgan("combined"));
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+route(app);
+
+app.listen(PORT, () => {
+  console.log(`BE-TEST app listening on port ${PORT}`)
+})

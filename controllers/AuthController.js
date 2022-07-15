a0fa5c474f5ab587066bf7bf1735d53ec8daae97
TanThanh-CityBoi <tanthanhe@gmail.com>
2022-07-15 13:15:02 +0700

    config db

---
+++ controllers/AuthController.js
@@ -0,0 +1,18 @@
+
+
+
+class AuthController {
+    register = async (req, res) => {
+
+    }
+
+    login = async (req, res) => {
+
+    }
+
+    resetPassword = async (req, res) => {
+
+    }
+}
+
+module.exports = new AuthController();
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000; // port number
 const app = express()
 const route = require("./routers/index"); // router impl
+const query = require("./configDB")
 var morgan = require("morgan");
 
 app.use(morgan("combined"));
@@ -11,6 +12,8 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 route(app);
+
+
 
 app.listen(PORT, () => {
   console.log(`BE-TEST app listening on port ${PORT}`)
